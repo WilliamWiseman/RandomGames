9d612febc5202f607eaaff638f8161773850375a
William Wiseman <williamwiseman10@icloud.com>
2024-12-27 18:48:31 -0500

    ooga booga

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const Pool = require("pg").Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: 'db',
+    database: 'nama_db_kamu',
+    password: 'example',
+    port: 5432
+});
 
 
 app.get("/", function(req, res) {
@@ -16,6 +24,24 @@ app.get("/snake", function(req, res){
 
 app.get("/willaga", function(req, res){
     res.sendFile(path.join(__dirname, 'views', '../Willaga.html'));
+
+});
+
+app.get("/toDo", function(req, res){
+    res.sendFile(path.join(__dirname, 'views', '../ToDoListFront.html'));
+
+});
+
+app.get("/toDo/API", function(req, res){
+
+});
+
+app.put("/toDo/API", function(req, res){
+
+});
+
+app.post("/toDo/API", function(req, res){
+    //INSERT INTO `todo` (`taskName`,`isDone`) VALUES ( 'Give tables rights', false);
 
 });
 
